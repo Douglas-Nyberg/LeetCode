8f377c89efded87775a5eaf8432f8513a8581255
Douglas Nyberg <71201490+Douglas-Nyberg@users.noreply.github.com>
2022-09-20 20:31:51 -0600

    Add files via upload

---
+++ Two Sum/OnePass/OnePass.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+
+class OnePass {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> hash = new HashMap<>();
+        int[] soln = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            if (hash.containsKey(target - nums[i]) && i != hash.get(target - nums[i])) {
+                soln[0] = i;
+                soln[1] = hash.get(target - nums[i]);
+                return soln;
+            } else {
+                hash.put(nums[i], i);
+            }
+
+        }
+
+        return null;
+    }
+}
